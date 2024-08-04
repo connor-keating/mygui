4b50547722a9811a096df387bcc2b94856bb5aef
Connor <conkeating32@gmail.com>
2024-08-04 13:45:02 -0400

    Changing game resolution to 1920x1080.

--- src/application.h
+++ src/application.h
@@ -3,8 +3,8 @@
 #include "types.h"
 
 #define GAME_NAME L"MyGUI"
-#define GAME_WIDTH 640
-#define GAME_HEIGHT 360
+#define GAME_WIDTH 1920
+#define GAME_HEIGHT 1080
 
 typedef struct game_screen game_screen;
 struct game_screen
